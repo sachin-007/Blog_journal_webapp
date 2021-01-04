c5d37571a6876323f640ac190b9826be727c2afa
sach-007 <sachinbachhav600@gmail.com>
2021-01-04 11:27:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "so this web App is database connected Journal created for project purpose you can compose/create your own postes and post it here do some fun with this web App";
+const aboutContent = "This app is created by FRONTEND:- html ,css ,jquery ,bootstrap ,ejs , js   BACKEND:- Node.js of express and DATABASE:- mongodb Atlas  AND  Deployed with the heroku Server";
+const contactContent = "you can contact us on our official mail:- sachinbachhav.org@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://sachin600:sachin_600@cluster0.jipil.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+// app.listen(port);
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+
+
+});
